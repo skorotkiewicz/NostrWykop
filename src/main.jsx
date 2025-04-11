04c58b8962050c2672e76aacfa2d2c237d7dbe8e
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-04-11 23:53:35 +0200

    StrictMode only in prod

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./i18n";
-// import './index.css'
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// );
+const Root = () => {
+  const AppWithRouter = (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+  if (import.meta.env.DEV) {
+    return AppWithRouter;
+  }
+
+  return <StrictMode>{AppWithRouter}</StrictMode>;
+};
+
+createRoot(document.getElementById("root")).render(<Root />);
